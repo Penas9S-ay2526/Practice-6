608e3b081dbb60b7ca468644f74cdd1204f382f9
Penas9S-ay2526 <penas.kr@obmontessori.edu.ph>
2026-04-28 07:41:40 +0800

    loop

---
+++ main.js
@@ -0,0 +1,6 @@
+function toCount(){
+    var animals = ["Frog", "Chick", "Shark", "Dog"];
+    for (var i = 0; i < animals.length; i++) {
+        window.alert("This zoo contains a " + animals[i] + ".")
+    }
+}
